7064e07f3a053da051284bdf2e6b2751f2655c47
motokikasai <kmomoky@hotmail.com>
2020-04-23 17:31:04 +0200

    Experiment with cw-unmount + add bg image for outro

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
 import React from "react";
 import "./App.css";
 import Countdown from "./components/Countdown";
-import moment from "moment";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      count: moment().format("MMMM Do YYYY, h:mm:ss a"),
-    };
+  //   this.state = {
+  //     count: moment().format("LTS"),
+  //     timeout: true,
+  //   };
 
-    // this.counter = this.counter.bind(this);
-  }
-
-  componentDidMount() {
-    this.counterInterval = setInterval(() => {
-      this.setState((state) => {
-        return {
-          count: state.count,
-        };
-      });
-
-      // return moment().format("MMMM Do YYYY, h:mm:ss a");
-    }, 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.counterInterval);
-  }
+  //   // this.counter = this.counter.bind(this);
+  // }
 
   render() {
     return (
       <div className="App">
-        <Countdown counter={this.state.count} />
+        <Countdown />
+
+        <div className="awesome-page">AWESOME!!!</div>
       </div>
     );
   }
--- src/components/Countdown/index.js
+++ src/components/Countdown/index.js
@@ -1,10 +1,43 @@
 import React from "react";
+import moment from "moment";
 
 class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 10,
+      timeout: true,
+    };
+  }
+
+  componentDidMount() {
+    this.counterInterval = setInterval(() => {
+      this.setState((state) => {
+        return {
+          count: state.count,
+        };
+      });
+
+      // return moment().format("MMMM Do YYYY, h:mm:ss a");
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    console.log(this.state.count);
+    this.state.count && clearInterval(this.counterInterval);
+  }
+
   render() {
     return (
       <div className="container">
-        <section className="counter">{this.props.counter}</section>
+        <section className="counter">
+          {this.state.count}
+          <div className="days">155 days</div>
+          <div className="hours">1 days</div>
+          <div className="minutes">9 days</div>
+          <div className="seconds">2 days</div>
+        </section>
         <section className="content">
           <div className="main-header">Coming soon...</div>
           <div className="subheader">Stick around and be one of the first</div>
@@ -15,3 +48,6 @@ class Counter extends React.Component {
 }
 
 export default Counter;
+
+// CountDown.defaultProps = {}
+// CountDown.defaultProps = {title: ‘Coming soon’}
