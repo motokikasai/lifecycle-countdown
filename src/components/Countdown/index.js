2283cf6663199e0c0dfea85e55f17ed60fae1326
motokikasai <kmomoky@hotmail.com>
2020-04-23 14:14:28 +0200

    Create basic structures with counter & content sections with a few texts

---
+++ src/components/Countdown/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Counter extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <section className="counter">left sidebar</section>
+        <section className="content">
+          <div className="main-header">Coming soon...</div>
+          <div className="subheader">Stick around and be one of the first</div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Counter;
